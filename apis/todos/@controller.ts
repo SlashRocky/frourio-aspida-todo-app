2850317380b6f4a1a27e16efcd492a8076b2b721
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2020-06-01 14:42:20 +0900

    frourioからcreateControllerを、index.tsから型Methodsをインポートし、Methodsというコントローラーを作成

--- apis/todos/@controller.ts
+++ apis/todos/@controller.ts
@@ -1,3 +1,12 @@
-import { Todo } from './'
+import { createController } from 'frourio'
+import { Todo, Methods } from './'
 
 export const todos: Todo[] = []
+
+export default createController<Methods>({
+  get: () => ({ status: 200, body: todos }),
+  post: ({ body }) => {
+    const todo = { id: 'aa', label: body.label, done: false }
+    return { status: 201, body: todo }
+  }
+})
